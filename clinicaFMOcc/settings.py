36066a996a81090c2957ae78a144e9613a3c70b3
Moises-png993 <pr20028@ues.edu.sv>
2025-07-07 15:10:09 -0600

    se agrega swagger

--- clinicaFMOcc/settings.py
+++ clinicaFMOcc/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'clinicaApp', 
     'rest_framework', 
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- clinicaFMOcc/urls.py
+++ clinicaFMOcc/urls.py
@@ -3,6 +3,22 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from clinicaApp.views import PacienteViewSet, HistoriaClinicaViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API de Historias Clínicas",
+        default_version='v1',
+        description="Documentación de la API REST de tu proyecto en Django",
+        terms_of_service="https://www.ejemplo.com/terminos/",
+        contact=openapi.Contact(email="moises.perez@ejemplo.com"),
+        license=openapi.License(name="MIT"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 router = DefaultRouter()
 router.register(r'pacientes', PacienteViewSet)
@@ -13,4 +29,7 @@ urlpatterns = [
     path('api/', include(router.urls)), 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
